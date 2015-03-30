61ef64bdbca16cc50eff1a1f175375ef5c0b74b3
ubuntu <anja.dvr@gmail.com>
2015-03-30 11:48:43 +0300

    Added vk, facebook, twitter authorization

--- musicalbits/settings.py
+++ musicalbits/settings.py
@@ -29,6 +29,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SOCIAL_AUTH_FACEBOOK_KEY = '1432920113671735'
+SOCIAL_AUTH_FACEBOOK_SECRET = '6d16412f1d566380beb0567c2af23d57'
+SOCIAL_AUTH_TWITTER_KEY = 'MG9dG1uez9qA0zjs8eq9Me3nZ'
+SOCIAL_AUTH_TWITTER_SECRET = 'FPaz1XRFwhqMZoqrMTgSOG9cFlt6j3wVd3w9UMVtrbdODTmZez'
+SOCIAL_AUTH_VK_OAUTH2_KEY = '4852525'
+SOCIAL_AUTH_VK_OAUTH2_SECRET = 'FgU0zXnptaq0g9EPYNkC'
+
+
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,7 +44,27 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social.apps.django_app.default',
     'main',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+   'django.contrib.auth.context_processors.auth',
+   'django.core.context_processors.debug',
+   'django.core.context_processors.i18n',
+   'django.core.context_processors.media',
+   'django.core.context_processors.static',
+   'django.core.context_processors.tz',
+   'django.contrib.messages.context_processors.messages',
+   'social.apps.django_app.context_processors.backends',
+   'social.apps.django_app.context_processors.login_redirect',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'social.backends.twitter.TwitterOAuth',
+    'social.backends.facebook.FacebookOAuth2',
+    'social.backends.vk.VKOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+#LOGIN_REDIRECT_URL = '/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
--- musicalbits/urls.py
+++ musicalbits/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+#SOCIAL_AUTH_URL_NAMESPACE = 'social'
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'musicalbits.views.home', name='home'),
+    url(r'^$', 'main.views.home', name='home'),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+
+    #url('', include('social.apps.django_app.urls', namespace='social')),
 )
